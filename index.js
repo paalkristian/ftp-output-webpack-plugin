1aa055b3a1c8f4b8393caa84207f8721d5aa702b
shij <shijixttb@163.com>
2018-03-21 11:30:33 +0800

    [enhancement] support resolve ftp url path directly

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+const url = require('url')
 const FtpOutputFileSystem = require('./FtpOutputFileSystem')
 
-function FtpOutputPlugin(options) {
-  this.options = options
+function FtpOutputPlugin(ftp, options) {
+  if (typeof ftp === 'string') {
+    const urlObj = url.parse(ftp)
+    if (urlObj.protocol === 'ftp:') {
+      const userinfo = urlObj.auth.split(':')
+      this.options = Object.assign({
+        path: urlObj.path,
+        host: urlObj.host,
+        user: userinfo[0],
+        password: userinfo[1],
+        port: urlObj.port || 21
+      }, options)
+    }
+  } else {
+    this.options = ftp
+  }
 }
 
 FtpOutputPlugin.prototype.apply = function (compiler) {
+  if (!this.options) return
   compiler.plugin('environment', () => {
     compiler.outputFileSystem = new FtpOutputFileSystem(this.options, compiler)
   })
